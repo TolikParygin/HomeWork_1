7a1b3a31aad0545d5fa7f65c6042bebf90996602
Tolik <anatolijparygin988@gmail.com>
2022-11-27 00:15:48 +0300

    проверка на четность

---
+++ zadacha_3/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Clear();
+Console.Write("Введите число: ");
+int N =int.Parse(Console.ReadLine());
+if(N%2==0)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
