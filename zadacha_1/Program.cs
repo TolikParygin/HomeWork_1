123972edbadc75ec4c75339a292550e4bb1a309e
Tolik <anatolijparygin988@gmail.com>
2022-11-26 23:45:23 +0300

    max из двух чисел

---
+++ zadacha_1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Clear();
+Console.WriteLine("Введите два числа:");
+int nomber1 = int.Parse(Console.ReadLine());
+int nomber2 = int.Parse(Console.ReadLine());
+if(nomber1>=nomber2)
+    Console.WriteLine("max "+ nomber1);
+else
+    Console.WriteLine("max "+ nomber2);
