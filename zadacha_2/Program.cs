05bd66805288cc116b45e315c36a0278e8834ddb
Tolik <anatolijparygin988@gmail.com>
2022-11-27 00:08:32 +0300

    max из трех чисел

---
+++ zadacha_2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.WriteLine("Введите три числа:");
+int nomber1 = int.Parse(Console.ReadLine());
+int nomber2 = int.Parse(Console.ReadLine());
+int nomber3 = int.Parse(Console.ReadLine());
+int max=nomber1;
+if(nomber1>=max)
+    max=nomber1;
+if(nomber2>=max)
+    max=nomber2;
+if(nomber3>=max)
+    max=nomber3;
+Console.Write("max = "+ max);
